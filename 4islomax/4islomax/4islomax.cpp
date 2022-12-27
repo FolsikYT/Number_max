708af873e24ae360e344d05f7107df00d4d193eb
Patc <106960561+FolsikYT@users.noreply.github.com>
2022-12-27 14:48:04 +0300

    Add files via upload

---
+++ 4islomax/4islomax/4islomax.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "максимальное число: " << (max(a, b), c) <<endl;
+
+
+
+
+
+
+
+
+
+
+     cin.get();
+     cin.get();
+     return 0;
+}
